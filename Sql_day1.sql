0ea5f768bedca2e8a142ee1b321dd52b5d2cf727
amksirajulsulaiha19ug29-boop <amksirajulsulaiha.19ug29@gmail.com>
2026-08-11 12:04:46 +0530

    update file

---
+++ Sql_day1.sql
@@ -0,0 +1,15 @@
+show databases;
+create database BankingDB;
+use BankingDB;
+
+CREATE TABLE Customers (
+    CustomerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    Phone VARCHAR(15),
+    AccounCreationDate DATE
+);
+    
+describe Customers;
+
